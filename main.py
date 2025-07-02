20b2d41d231b4f8aa2c592d8c0dd27ba5561ba0c
TannerBebb <17tbebb@gmail.com>
2025-07-02 16:17:58 -0500

    I dont even really know what all I added.

--- main.py
+++ main.py
@@ -12,12 +12,26 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+"""
+    This is an event that will reply hello when a user types hello.
+"""
 @client.event
 async def on_message(message):
+    """
+        If i remember correctly, this command keeps the bot from replying to itself.
+    """
     if message.author == client.user:
         return
-
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
+    
+    """
+        This is a set of commands that will forward a message to a specific channel.
+    """
+    if message.content.startswith('!forward'):
+        #the parameters has the id for the bot testing channel.
+        specificChannel = client.get_channel(1387221514553921626)
+            
+    if specificChannel:
+        await specificChannel.send(message.content)
+        await message.channel.send("Message forwarded!")
 
 client.run(TOKEN)
