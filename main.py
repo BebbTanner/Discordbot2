49ce6e8b53f9f4d6e874498bfe7d959d68282da6
TannerBebb <17tbebb@gmail.com>
2025-07-04 16:26:34 -0500

    I commented out the section that would forward an image. I am not longer using it.

--- main.py
+++ main.py
@@ -31,12 +31,13 @@ async def on_message(message):
 
     """
         This is a command that will forward and image in the bot testing channel.
+    I commented this out, I am not using it for the time being.
     """
-    if message.attachments:
-        for attachment in message.attachments:
-            if attachment.content_type.startswith('image'):
-                targetChannel = client.get_channel(1387221514553921626)
-                await targetChannel.send(attachment.url)
+    #if message.attachments:
+    #    for attachment in message.attachments:
+    #        if attachment.content_type.startswith('image'):
+    #            targetChannel = client.get_channel(1387221514553921626)
+    #            await targetChannel.send(attachment.url)
 
     """
         This gets the message id for a message sent from a specific user, then
