c0f9491705473c0956c40768eb7db34ea1313dcd
TannerBebb <17tbebb@gmail.com>
2025-07-29 20:51:19 -0500

    I am starting from square one. What am I learning from this?

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+#Global variables
+botTesting = 1387221514553921626
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -16,17 +19,12 @@ async def on_message(message):
     #Keeps the bot from getting stuck in and infinite loop.
     if message.author == client.user:
         return
-    
-    #This is getting the channelID and storing it to the new channel variable.
-    newChannel = client.get_channel(1387221514553921626)
 
-    #This is a loop that should be responding only to me in discord.
-    if message.author.id == tannerID and newChannel == 1387221514553921626:
-        targetChannel = client.get_channel(1309632891080413294)
-        
-        if targetChannel:
-            await targetChannel.send("Fuck you")
-
+    """
+        If a message starts with hello, the bot will reply with hello.
+    """
+    if message.content.startswith("hello"):
+        await message.channel.send("hello")
 
 
 client.run(botToken)
