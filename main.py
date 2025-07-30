cd5f0a20b29a353828fda4f4649c795ad6b32be3
TannerBebb <17tbebb@gmail.com>
2025-07-30 16:55:30 -0500

    Uh i dont really know on this one. I am taking notes on a noteboook.

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 """
+tannerID = 205792085796716544
+
+botTesting = 1387221514553921626
+"""
+
+"""
     When the program executes, this sends a message to the console that 
 lets you know that it logged in correctly.
 """
@@ -14,11 +20,21 @@ lets you know that it logged in correctly.
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+
+
 @client.event
 async def on_message(message):
+    tannerID = 205792085796716544
 
     #Keeps the bot from getting stuck in and infinite loop.
     if message.author == client.user:
         return
+    
+    #This should reply to me and @ my discord account
+    if message.author.id == tannerID:#user id to foward this will be mine
+        destination_channel = client.get_channel(1387221514553921626)#destination channel
+        await destination_channel.send(f"{message.author.name}: {message.content}")
+    
+    await client.process_commands(message)
 
 client.run(botToken)
