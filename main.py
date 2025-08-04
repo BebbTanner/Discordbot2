e06d5e974e1f28d48cdc086eee185b6c7c42e41b
TannerBebb <17tbebb@gmail.com>
2025-08-04 15:12:50 -0500

    Idk what all I added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 """
     So the bot logs the reply from quote bot, but now I need to send the image that 
 quote bot is generating.
+
+    Im still not sure as to why grok will not repost the image that quotebot generates.
+It might be because I don't have the exact file type that is created with make it a quote.
 """
 
 from discordBot import *
@@ -31,7 +34,6 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    tannerID = 205792085796716544
     makeItAQuote = 949479338275913799
 
     #Keeps the bot from getting stuck in and infinite loop.
@@ -40,10 +42,33 @@ async def on_message(message):
     
     #Any time I type anything in any channel, This post my tag with my message 
     #in the bot testing channel.
-    if message.author.id == makeItAQuote:#user id to foward this will be mine
+    if message.author.id == makeItAQuote:
         destination_channel = client.get_channel(1387221514553921626)#destination channel
-        await destination_channel.send(f"{message.author.name}: {message.content}")
-    
-    await client.process_commands(message)
+        await destination_channel.send("Image from quotebot is generating.")
+  
 
+    
+    # Check if the message has attachments (potential images)
+    if message.attachments:
+        for attachment in message.attachments:
+            # Check if the attachment is an image (you might want more robust checks)
+            if attachment.content_type and 'PNG' in attachment.content_type:
+                target_channel = client.get_channel(1387221514553921626)
+                if target_channel:
+                    # Convert the attachment to a discord.File and send it
+                    await target_channel.send(f"Image from {message.author.display_name}:", file=await attachment.to_file())
+                else:
+                    print(f"Target channel with ID {1387221514553921626} not found.")
+    
+    await client.process_commands(message) # Important for command handling
+
+client.event
+async def on_message(message):
+    # Ignore messages from the bot itself
+    if message.author == client.user:
+        return
+
+
+
+    
 client.run(botToken)
