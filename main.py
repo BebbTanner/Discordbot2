ff4e43fc85b2687c8510805c87a5293c387c11ab
TannerBebb <17tbebb@gmail.com>
2025-11-09 16:13:24 -0600

    I did a factory reset

--- main.py
+++ main.py
@@ -16,57 +16,13 @@ intents.message_content = True
 client = commands.Bot(command_prefix="!", intents=intents)
 
 """
-tannerID = 205792085796716544
-makeItAQuote = 949479338275913799
-
-botTesting = 1387221514553921626
-"""
-
-"""
     When the program executes, this sends a message to the console that 
 lets you know that it logged in correctly.
 """
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
-
-
-@client.event
-async def on_message(message):
-    makeItAQuote = 949479338275913799
-
-    #Keeps the bot from getting stuck in and infinite loop.
-    if message.author == client.user:
-        return
-    
-    #Any time I type anything in any channel, This post my tag with my message 
-    #in the bot testing channel.
-    if message.author.id == makeItAQuote:
-        destination_channel = client.get_channel(1387221514553921626)#destination channel
-        await destination_channel.send("Image from quotebot is generating.")
-  
-
-    
-    # Check if the message has attachments (potential images)
-    if message.attachments:
-        for attachment in message.attachments:
-            # Check if the attachment is an image (you might want more robust checks)
-            if attachment.content_type and 'PNG' in attachment.content_type:
-                target_channel = client.get_channel(1387221514553921626)
-                if target_channel:
-                    # Convert the attachment to a discord.File and send it
-                    await target_channel.send(f"Image from {message.author.display_name}:", file=await attachment.to_file())
-                else:
-                    print(f"Target channel with ID {1387221514553921626} not found.")
-    
-    await client.process_commands(message) # Important for command handling
-
-client.event
-async def on_message(message):
-    # Ignore messages from the bot itself
-    if message.author == client.user:
-        return
 
 
 
