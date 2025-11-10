ab56d97a8ab66d9cc38c5bda399527dde7f9179d
TannerBebb <17tbebb@gmail.com>
2025-11-09 18:03:05 -0600

    I got my true false thingy to work correctly.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ trueResponses = [
 falseResponses = [
     "Kill yourself.",
     "You could not be farther from the truth.",
-    "At no point in your rambling, incoherent response were ryou even close to anything that could be considered a rational thought. Everyone is this room is now dumber for hearing it. I award you no points, and may God have mercy on your soul.",
+    "At no point in your rambling, incoherent response were you even close to anything that could be considered a rational thought. Everyone is this room is now dumber for hearing it. I award you no points, and may God have mercy on your soul.",
 ]
 
 """
@@ -44,26 +44,20 @@ async def on_message(message):
     This will reply to anyone that mentions the discordbot.
     '''
     if message.mentions and message.mentions[0].id == client.user.id:
-        if 'true' in message.content:
-            await message.channel.send(random.choice(trueResponses))
-        
-        if 'True' in message.content:
-            await message.channel.send(random.choice(trueResponses))
+        if 'rue' in message.content:
+            randomint = random.randint(1,2)
 
-        if 'false' in message.content:
-            await message.channel.send(random.choice(falseResponses))
+            if randomint == 1:
+                await message.channel.send(random.choice(trueResponses))
 
-        if 'False' in message.content:
-            await message.channel.send(random.choice(falseResponses))
-
+            elif randomint == 2:
+                await message.channel.send(random.choice(falseResponses))
+    
     '''
     This is an if statement that will look for the word steve in a message.
     If it contains this keyword it will put in general kill steve.
     '''
     if 'steve' in message.content:
-        await message.channel.send("Kill steve")
-
-    if 'feet' in message.content:
         await message.channel.send("Kill steve")
 
 client.run(botToken)
